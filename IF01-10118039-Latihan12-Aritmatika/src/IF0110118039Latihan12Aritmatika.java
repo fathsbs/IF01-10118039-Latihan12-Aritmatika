a54a32b4017f00f272c69a24b38ba9977ccd5560
fathsbs <55565303+fathsbs@users.noreply.github.com>
2019-09-23 20:11:48 +0700

    Add files via upload

---
+++ IF01-10118039-Latihan12-Aritmatika/src/IF0110118039Latihan12Aritmatika.java
@@ -0,0 +1,30 @@
+/*
+ * NAMA              : FATAHILLAH SATRIA BIMA SENO
+ * KELAS             : IF-01
+ * NIM               : 10118039
+ * DESKRIPSI PROGRAM :   
+ */
+
+public class IF0110118039Latihan12Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b/a = " + (b / a));
+        System.out.println("b%a = " + (b % a));
+        System.out.println("c%a = " + (c % a));
+        System.out.println("a++ = " + (a++));
+        System.out.println("a-- = " + (a--));
+        System.out.println("d++ = " + (d++));
+        System.out.println("++d = " + (++d));
+    }
+
+}
